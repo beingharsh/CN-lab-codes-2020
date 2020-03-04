662f1183eb80ae066ece0e1f57c361a62a8092d8
Aman Jain <amanforcode@gmail.com>
2020-03-04 22:58:12 +0530

    New

--- CN lab wed 8-10 week 1 and 2/first/out/gcc-release/fnode.d
+++
@@ -1,8 +0,0 @@
-out/gcc-release//fnode.o: fnode.cc fnode.h \
- /opt/omnetpp-5.4.1/include/omnetpp.h MyMsg_m.h
-
-fnode.h:
-
-/opt/omnetpp-5.4.1/include/omnetpp.h:
-
-MyMsg_m.h:
--- ass 3/out/gcc-release/nl.d
+++
@@ -1,8 +0,0 @@
-out/gcc-release//nl.o: nl.cc nl.h nl_msg_m.h \
- /opt/omnetpp-5.4.1/include/omnetpp.h
-
-nl.h:
-
-nl_msg_m.h:
-
-/opt/omnetpp-5.4.1/include/omnetpp.h:
